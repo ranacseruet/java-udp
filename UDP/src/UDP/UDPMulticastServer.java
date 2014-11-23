d6f8468e7b97b19028f764601ba58e212c5a4314
Eftakhairul Islam <eftakhairul@gmail.com>
2014-11-23 16:56:00 -0500

    MulticastUDPServer is done

---
+++ UDP/src/UDP/UDPMulticastServer.java
@@ -0,0 +1,20 @@
+package UDP;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+
+public class UDPMulticastServer extends UDPServer {
+	
+	public MulticastSocket socket;	
+
+	public UDPMulticastServer(String host, int port) throws IOException {
+		super(host, port);	
+		
+		this.socket = new MulticastSocket(this.port);
+		
+		// must bind receive side
+		socket.joinGroup(InetAddress.getByName(this.host)); 
+	}
+}
